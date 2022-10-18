d1030f9273b23ae1a3bf7e74e7e23c8366179707
matheusgarcia37 <matheusgarcia37@hotmail.com>
2022-10-18 09:05:11 -0300

    att

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import TodoRouter from './routes/todo.routes';
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req: any, res: any) => {
   res.send('Hello World!');
